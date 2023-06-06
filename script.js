c7e42bbff1eb3b3c8ab23acb6dd03e277656c6c6
DavidJBlethen <55075983+DavidJBlethen@users.noreply.github.com>
2023-06-05 22:42:39 -0500

    Update Restaurant List/Remove inside jokes for public post

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let defaultOptions = ["Cane's", "Pizza Ranch", "El Caballo De Oro", "El Vallarta", "Moe's", "Sakura", "Texas Roadhouse", "Fuzzy's", "Taco Bell. Get frickin' rekt, Tanner", "Fazoli's. Get frickin' rekt Tanner and DJ", "Trackside Burgers", "Freddy's", "Steak 'n Shake", "Mac Cheesy's", "IHOP", "Arby's", "Wendy's. Get frickin' rekt, DJ", "Bricktown Brewery","Culver's", "Mall","Qdoba", "Chipotle","Braum's", "Tropicana", "McAllister's", "Del Rio","Seth's house.", "Outback", "Magic Noodle", "Thai Time", "Pho Forever", "Pho Saigon", "Extra Option 1", "Extra Option 2", "Extra Option 3"]
+let defaultOptions = ["Cane's", "Pizza Ranch", "El Caballo De Oro", "El Vallarta", "Moe's", "Sakura", "Texas Roadhouse", "Fuzzy's", "Taco Bell", "Fazoli's", "Trackside Burgers", "Freddy's", "Steak 'n Shake", "Mac Cheesy's", "IHOP", "Arby's", "Wendy's", "Bricktown Brewery","Culver's", "Mall","Qdoba", "Chipotle","Braum's", "Tropicana", "McAllister's", "Del Rio","Seth's house.", "Outback", "Magic Noodle", "Thai Time", "Pho Forever", "Pho Saigon", "Just go home."]
 
 let fate = document.querySelector("#fate")
 let options = document.querySelector("#options")

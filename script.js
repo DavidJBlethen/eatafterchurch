7d765970acad28e53631666ca2de9f5a67dedbc9
DavidJBlethen <55075983+DavidJBlethen@users.noreply.github.com>
2023-12-17 11:57:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let defaultOptions = ["Cane's", "Pizza Ranch", "El Caballo De Oro", "El Vallarta", "Moe's", "Sakura", "Texas Roadhouse", "Fuzzy's", "Taco Bell", "Fazoli's", "Trackside Burgers", "Freddy's", "Steak 'n Shake", "Mac Cheesy's", "IHOP", "Arby's", "Wendy's", "Bricktown Brewery","Culver's", "Mall","Qdoba", "Chipotle","Braum's", "Tropicana", "McAllister's", "Del Rio","Seth's house.", "Outback", "Magic Noodle", "Thai Time", "Pho Forever", "Pho Saigon", "Just go home."]
+let defaultOptions = ["Cane's", "Pizza Ranch", "El Caballo De Oro", "El Vallarta", "Moe's", "Sakura", "Texas Roadhouse", "Fuzzy's", "Taco Bell", "Fazoli's", "Trackside Burgers", "Freddy's", "Steak 'n Shake", "Mac Cheesy's", "IHOP", "Arby's", "Wendy's", "Bricktown Brewery","Culver's", "Mall","Qdoba", "Chipotle","Braum's", "Tropicana", "McAllister's", "Del Rio","Seth's house.", "Outback", "Magic Noodle", "Thai Time", "Pho Forever", "Pho Saigon", "Just go home.", "Fast and pray, you cow."]
 
 let fate = document.querySelector("#fate")
 let options = document.querySelector("#options")
